f109434b5b1ee5265c7ac797ee21228200159492
Dekevion Wade <Dekevionwade@yahoo.com>
2019-08-09 15:59:39 -0500

    loop isnt fixed

---
+++ index.js
@@ -0,0 +1,42 @@
+var ask = prompt("Welcome. press 1 to count. press 2 to use a calculator. press q to quit."); {
+var b = 1;
+if (ask ==="1") {
+	var c = parseInt(prompt("Enter a number(this will count by ones leading up that #)"));
+ while(b < c) {
+ 	b
+ }
+
+
+	}
+
+
+if (ask==="2") {
+	var num1 = prompt("select 'a' for addition, or 's'for subtraction");
+} 
+
+
+if (num1 === "a") {
+	var add1 = parseInt(prompt('Enter a number'));
+var add2 = parseInt(prompt("Enter a number"));
+var sum = alert(add1 + add2);
+
+} else {
+
+  (num1 ==="s") 
+	var sub = prompt("Enter number");
+var sub2 = prompt("Enter number");
+var sum2 = alert(Number(sub) - Number(sub2));
+
+}
+	
+
+
+var askMe = ""
+while (askMe !== "q"); {
+
+} 
+
+
+
+
+}
